2bb7145c9d27edc092d1b896105284051692d435
Oleksandr-N <hekp1984@gmail.com>
2023-03-13 23:47:52 +0200

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
+import Profile from './Profile';
+import { user } from '../data';
+
 function App() {
   return (
     <>
-      <hi>dddd</hi>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </>
   );
 }
